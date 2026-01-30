465cba4357ab7e15899cf828b0c1db611da0b05e
HeroKyou25 <d1041231088@student.untan.ac.id>
2026-01-30 09:37:56 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Http\Controllers\DashboardAdminController;
 use App\Http\Controllers\PrintStationController;
 use App\Livewire\UserUpload;
@@ -12,3 +13,6 @@ Route::get('/', function () {
 Route::get('/station', [PrintStationController::class, 'index']);
 Route::get('/station/{printfile}', [PrintStationController::class, 'show'])->name('station.show');
 Route::get('/admin/dashboard', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
+Route::get('/', [PrintController::class, 'index']);
+Route::get('/upload', [PrintController::class, 'uploadPage']);
+Route::post('/upload', [PrintController::class, 'store']);
