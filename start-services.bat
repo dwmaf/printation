466546a223e84b5fbb830187809ebeea5efb63ec
nana72848e <d1041211005@student.untan.ac.id>
2026-02-23 13:20:25 +0700

    perbarui service bat

--- start-services.bat
+++ start-services.bat
@@ -4,10 +4,10 @@ echo Menjalankan Printation Services...
 cd c:\laragon\www\print-app
 
 :: Start Laravel Web Server di background log minimal
-start "Printation Web Server" /MIN php artisan serve --host=0.0.0.0 --port=8000
+start "Printation Web Server" php artisan serve --host=0.0.0.0 --port=8000
 
 :: Start Reverb WebSocket di background
-start "Printation WebSocket" /MIN php artisan reverb:start --host=0.0.0.0 --port=8080
+start "Printation WebSocket" php artisan reverb:start --host=0.0.0.0 --port=8080
 
 :: (Opsional) Langsung buka Google Chrome Kiosk Mode ke halaman print station
 :: start chrome --kiosk "http://localhost:8000/station"
